b495bf8f11146cc1349c497fb1a8c4855dad7e30
unknown <mara.ruiz7470@alumnos.udg.mx>
2022-09-21 21:12:07 -0500

    front

---
+++ src/front/pages/modal.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react'
+import Portal from './portal'
+
+export default class Modal extends Component{
+    render(){
+        
+       const {children, toggle, active} = this.props;
+       return(
+        <Portal>
+            {active &&  (
+                <div style={styles.wrapper}>
+                    <div style={styles.window}>
+                        <button style={styles.closeBtn} onClick={toggle}>X</button>
+                        <div> {children} </div>
+                    </div>
+
+                </div>
+            )
+            }
+        </Portal>
+       )   
+        
+    }
+}
+
+const styles = {
+    wrapper: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        hight: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItem: 'center',
+    },
+    window: {
+        position: 'relative',
+        background: '#fff',
+        borderRadius: 10,
+        padding: 15, 
+        boxShadow: '2px 2px 10px  rgba(0,0,0,0.3)',
+        zIndex:10,
+        top: 90,
+        minWidth: 400,
+        
+    },
+    closeBtn: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        background: 'none',
+        border: 'none',
+    }
+};
---
+++ src/front/pages/perfil.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react"
+import '../css/perfil.css'
+import user from '../../media/user.png';
+import notifications from '../../media/notifications.png';
+import search from '../../media/search.png';
+import messanger from '../../media/messanger.png';
+import fondo from '../../media/fondo-3.jpg';
+
+
+function Perfil() {
+  return (
+    <div>
+      <header className="principal-header">
+        <a className='logo-link' href='/'>tutorate</a>
+        <div className='search'>
+        <input className='search-input' placeholder='Buscar...' type="text"></input>
+        <button className='search-icon'>
+          <img className='search-imag' src={search} />
+        </button>
+        </div>
+        <button className='boton-circular'> <img className='icon' src={user} /> </button>
+        <button className='boton-circular'> <img className='icon' src={notifications} /> </button>
+        <button className='boton-circular'> <img className='icon' src={messanger} /> </button>
+        <div className='linea' />
+      </header>
+      <body className='principal-body'>
+        <img className="fondo" src={fondo} />
+        <button className='boton-circular-perfil'> <img className='icon-perfil' src={user} /> </button>
+        <h1 style={{textAlign:'center', marginTop:'100px'}}>Mara Alessandra Ruiz Gonzalez</h1>
+      </body>
+
+    </div>
+  );
+}
+
+export default Perfil;
